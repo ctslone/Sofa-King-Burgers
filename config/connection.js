c63ef8fa4b06be6a733d84b9a2b25258fecfbc75
ctslone <ctslone27@gmail.com>
2019-12-17 10:41:23 -0500

    adding jaws db credentials

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // dependencies
 var mysql = require('mysql');
 // create connection varible holding an object of login info
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Uncbootcamp1!",
-    database: "sofaking_db"
-  });
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL):
+}
+else {
+   connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Uncbootcamp1!",
+        database: "sofaking_db"
+      });
+};
+
 // conencting to the DB
 connection.connect(function(err) {
     if (err) {
