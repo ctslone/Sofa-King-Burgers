15b4329f7f44324dee89326730009b6046de51ab
Charlie <ctslone27@gmail.com>
2020-07-10 19:15:19 -0400

    removing old function not needed.

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,5 @@
 var connection = require("../config/connection.js")
 
-function printQuestionMarks(num) {
-    var questionArray = [];
-    for (var i = 0; i < num; i++) {
-        questionArray.push("?");
-    }
-    return questionArray.toString();
-}
 
 function objToSql(ob) {
     var sqlArr = [];
@@ -15,12 +8,10 @@ function objToSql(ob) {
         var value = ob[key];
         // check to skip hidden properties
         if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            // if string with spaces, add quotations
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
             }
-            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-            // e.g. {sleepy: true} => ["sleepy=true"]
             sqlArr.push(key + "=" + value);
         }
     }
